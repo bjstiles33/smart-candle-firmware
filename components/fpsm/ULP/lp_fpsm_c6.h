5984e669a172a0dda1ac6079b45d1ccccfaf29cb
bjstiles33 <72944361+bjstiles33@users.noreply.github.com>
2025-09-09 15:12:10 -0400

    Add files via upload

---
+++ components/fpsm/ULP/lp_fpsm_c6.h
@@ -0,0 +1,38 @@
+
+/* ULP variable definitions for the compiler.
+ * This file is generated automatically by esp32ulp_mapgen.py utility.
+ */
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern uint32_t ulp_UART1;
+extern uint32_t ulp_LP_TIMER;
+extern uint32_t ulp_LP_I2C;
+extern uint32_t ulp_ulp_ulp_base_brightness;
+extern uint32_t ulp_LPPERI;
+extern uint32_t ulp_reset_vector;
+extern uint32_t ulp_ulp_ulp_cfg_shimmer_amp;
+extern uint32_t ulp___stack_top;
+extern uint32_t ulp_ulp_ulp_cfg_drift_div;
+extern uint32_t ulp_PMU;
+extern uint32_t ulp_ulp_ulp_cfg_env_cycles;
+extern uint32_t ulp_ulp_ulp_cfg_frame_pause;
+extern uint32_t ulp_ulp_ulp_cfg_target_stride;
+extern uint32_t ulp_ulp_ulp_run_flag;
+extern uint32_t ulp_ulp_ulp_cfg_lvl_smooth;
+extern uint32_t ulp_LP_UART;
+extern uint32_t ulp_ulp_ulp_rng_state;
+extern uint32_t ulp_ulp_ulp_cfg_jitter_hold;
+extern uint32_t ulp_LP_AON;
+extern uint32_t ulp_UART0;
+extern uint32_t ulp___mtvec_base;
+extern uint32_t ulp_LP_IO;
+extern uint32_t ulp_ulp_ulp_cfg_breath_smooth;
+extern uint32_t ulp_LP_CLKRST;
+extern uint32_t ulp_PCR;
+
+#ifdef __cplusplus
+}
+#endif
